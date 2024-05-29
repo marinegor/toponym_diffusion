7ce7df1abed3813888692ebb9b793c9aa1d3801c
Egor Marin <e.marin@enpicom.com>
2024-05-29 15:10:38 +0000

    First version draft -- dutch names

---
+++ models.py
@@ -0,0 +1,153 @@
+from torchtyping import TensorType
+import torch
+
+SequenceBatch = TensorType["batch", "channels", "L", torch.float]
+Timestamps = TensorType["batch", torch.long]
+
+
+class Scheduler:
+    def __init__(self, T: int, beta_0: float = 1e-4, beta_T: float = 2e-2):
+        """Initialize a scheduler.
+
+        Note that we pre-compute self._alphas, self._betas and self._alphabars:
+        they are pre-defined for each timepoint, hence we can initialize them once
+        and then sample given a timepoint tensor.
+
+        Parameters
+        ----------
+        T : int
+            total number of timepoints to sample
+        beta_0 : float, optional
+            value when t=0, by default 1e-4
+        beta_T : float, optional
+            value when t=T, by default 2e-2
+        """
+        assert beta_0 <= beta_T, "beta_0 is greater than beta_T"
+        assert 0 < beta_0 < 1 and 0 < beta_T < 1
+        self.T = T
+        self.beta_0 = beta_0
+        self.beta_T = beta_T
+
+        # we need:
+        #  - "noise power" betas
+        #  - alphas = 1-betas
+        #  - alpha_bars = cumprod(alphas)
+
+        self._betas = torch.linspace(beta_0, beta_T, T + 1)
+        self._alphas = 1 - self._betas
+        self._alphabars = torch.cumprod(self._alphas, dim=-1)
+
+    def _check_input(self, t: Timestamps):
+        """Checks if given timestamps are compatible with the current scheduler
+
+        Parameters
+        ----------
+        t : Timestamps
+            input timestamps
+        """
+        assert (
+            0 <= t.min().item() and t.max().item() <= self.T
+        ), (
+            f"Timestamps don't match maximum T={self.T}: {t.min()=}, {t.max()=}"
+        )  # check that there'll be no indexing error
+
+    def get_betas(self, t: Timestamps) -> TensorType["batch", torch.float]:  # noqa: F821
+        """Return $\beta_t$ for all timestamps in `t`.
+
+        Returns
+        -------
+        TensorType["batch", torch.float]
+            Output betas
+        """
+        self._check_input(t)
+        return self._betas[t]
+
+    def get_alphas(self, t: Timestamps) -> TensorType["batch", torch.float]:  # noqa: F821
+        """Return $\alpha_t$ for all timestamps in `t`.
+
+        Returns
+        -------
+        TensorType["batch", torch.float]
+            Output alphas
+        """
+        self._check_input(t)
+        return self._alphas[t]
+
+    def get_alphabar(self, t: Timestamps) -> TensorType["batch", torch.float]:  # noqa: F821
+        """Return $\alphabar_t$ for all timestamps in `t`.
+        This is used later to generate t-th sample in closed form,
+        instead of sampling it iteratively.
+
+        Returns
+        -------
+        TensorType["batch", torch.float]
+            Output alphabar parameters.
+        """
+        self._check_input(t)
+        return self._alphabars[t]
+
+
+class ForwardDiffusionProcess:
+    def __init__(self, scheduler: Scheduler):
+        self.scheduler = scheduler
+
+    def sample_t_plus_1(self, x_t: SequenceBatch, t: Timestamps) -> SequenceBatch:
+        """Samples next image for all images in batch, given timestamps.
+
+        Timestamps are only needed to get the beta parameters -- in a simplest
+        case, they are constant and can safely be omited. However, for more complex
+        schedulers, they must be present to sample correctly.
+
+        Here we're using the following equation:
+
+        $$
+        q(x_t | x_{t-1}) ~ N(\sqrt(1 - \beta_t)x_{t-1}, \beta_t I)
+        $$
+
+        which is essentially a single step of the diffusion process.
+
+        Parameters
+        ----------
+        x_t : SequenceBatch
+            input images
+        t : Timestamps
+            input timestamps for each image
+
+        Returns
+        -------
+        SequenceBatch
+            transformed images
+        """
+        beta_t: TensorType["batch", torch.float] = self.scheduler.get_betas(t)  # noqa: F821
+        beta_t: TensorType["batch", 1, 1, 1, torch.float] = beta_t.view(  # noqa: F821
+            beta_t.shape[0], *[1 for _ in x_t.shape[1:]]
+        )  # the .view() is to make pytorch broadcasting work
+
+        mean: SequenceBatch = (1 - beta_t).sqrt() * x_t
+        std: SequenceBatch = torch.randn_like(x_t) * beta_t
+        return mean + std
+
+    def sample_T(self, x_0: SequenceBatch, t: Timestamps) -> SequenceBatch:
+        """Sample a certain timepoint in a closed form
+
+        Parameters
+        ----------
+        x_0 : SequenceBatch
+            starting (unperturbed) image
+        t : Timestamps
+            timestamps
+
+        Returns
+        -------
+        SequenceBatch
+            output, perturbed image
+        """
+        alpha_bar_t: TensorType["batch", torch.float] = self.scheduler.get_alphabar(t)  # noqa: F821
+        alpha_bar_t: TensorType["batch", 1, 1, 1, torch.float] = alpha_bar_t.view(  # noqa: F821
+            alpha_bar_t.shape[0], *[1 for _ in x_0.shape[1:]]
+        )
+
+        mean: SequenceBatch = alpha_bar_t.sqrt() * x_0
+        std: SequenceBatch = torch.randn_like(x_0) * (1 - alpha_bar_t)
+
+        return mean + std
---
+++ utils.py
@@ -0,0 +1,144 @@
+import polars as pl
+import torch
+
+
+class OutdatedError(Exception):
+    pass
+
+
+def single_offset_expr(offset: int, context_length: int, start_or_end_token: str):
+    name = "sequence"
+    colname = f"ch{context_length + 1 - offset:0{context_length}}"
+    assert len(start_or_end_token) == 1
+    x = start_or_end_token
+
+    return colname, (
+        pl.concat_str(
+            [pl.lit(x) for _ in range(offset)]
+            + [pl.col(name)]
+            + [pl.lit(x) for _ in range(context_length + 1 - offset)]
+        )
+        .alias("sequence")
+        .str.reverse()
+        .str.slice(offset=context_length)
+        .str.reverse()
+        .alias(colname)
+        .str.split(by="")
+    )
+
+
+class Tokenizer:
+    def __init__(
+        self,
+        alphabet: str,
+        max_len: int,
+        start_token: str = "<",
+        end_token: str = ">",
+        pad_token: str = ".",
+    ):
+        assert len(start_token) == len(end_token) == len(pad_token) == 1
+        assert not any((t in alphabet for t in (start_token, end_token, pad_token)))
+        alphabet = f"{start_token}{end_token}{pad_token}{alphabet}"
+
+        self.alphabet = sorted(list(alphabet))
+        self.start_token = start_token
+        self.end_token = end_token
+        self.pad_token = pad_token
+        self.max_len = max_len
+        self._stoi = {char: idx for idx, char in enumerate(alphabet)}
+        self._itos = {idx: char for idx, char in enumerate(alphabet)}
+
+    @property
+    def stoi(self):
+        return self._stoi
+
+    @property
+    def itos(self):
+        return self._itos
+
+    def encode(self, df: pl.DataFrame, colname: str = "sequence") -> torch.Tensor:
+        """Returns OHE encodings of input sequence, padded to max length.
+
+        Parameters
+        ----------
+        df : pl.DataFrame
+            input dataframe
+        colname : str, optional
+            column to encode, by default "sequence"
+        """
+        df.with_columns(
+            ...
+        )
+
+    def into_ngrams(self, df: pl.LazyFrame, context_length: int) -> pl.DataFrame:
+        raise OutdatedError
+        offset_adding_expr = reversed(
+            [
+                single_offset_expr(
+                    offset=i,
+                    context_length=context_length,
+                    start_or_end_token=self.start_or_end_token,
+                )
+                for i in range(context_length + 1)
+            ]
+        )
+        colnames, expressions = list(zip(*offset_adding_expr))
+        return (
+            df.with_columns(*expressions)
+            .select(colnames)
+            .explode(colnames)
+            .filter(*(pl.col(colname).is_in(self.alphabet) for colname in colnames))
+        )
+
+    def ngrams2torch(
+        self,
+        ngrams: pl.LazyFrame | pl.DataFrame,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        raise OutdatedError
+        # ensure non-lazyness
+        ngrams = ngrams.lazy().collect()
+        stoi = self.stoi
+
+        renames = {
+            colname: pl.col(colname).replace(stoi).cast(pl.Int8)
+            for colname in ngrams.columns
+        }
+        ngrams = ngrams.with_columns(**renames)
+
+        last_col = sorted(ngrams.columns)[-1]
+        ys = ngrams.get_column(last_col).to_torch()
+
+        xs_colnames = sorted(ngrams.columns)[:-1]
+        xs = ngrams.select(xs_colnames).to_torch()
+
+        return xs, ys
+
+    def ngrams2torch_ohe(
+        self,
+        ngrams: pl.LazyFrame | pl.DataFrame,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        raise OutdatedError
+        stoi = self.stoi
+        ngrams = ngrams.lazy().collect()
+        last_col = sorted(ngrams.columns)[-1]
+        ys = torch.tensor(
+            [
+                stoi[char]
+                for char in ngrams.lazy().collect().get_column(last_col).to_list()
+            ]
+        ).int()
+
+        # ensure that all valid amino acids are present, and the order is appropriate
+        xs_colnames = sorted(ngrams.columns)[:-1]
+        dummy_xs_colnames = [
+            f"{colname}_{aa}" for colname in xs_colnames for aa in self.alphabet
+        ]
+
+        ngrams = ngrams.to_dummies()
+        for colname in dummy_xs_colnames:
+            if colname not in ngrams.columns:
+                ngrams = ngrams.with_columns(pl.lit(0, dtype=pl.Int8).alias(colname))
+
+        xs = ngrams.select(dummy_xs_colnames).to_torch().float()
+
+        return xs, ys
