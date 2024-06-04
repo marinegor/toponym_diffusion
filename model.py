94c7940b8ec93708b95efa79c2ddbf8c69e899a7
Egor Marin <e.marin@enpicom.com>
2024-06-04 11:17:15 +0000

    Add convolutional text model with time embedings

--- model.py
+++ model.py
@@ -2,9 +2,8 @@ import math
 
 import torch
 from torch import nn
-from torchtyping import TensorType
-
-T = TensorType
+import torch.nn.functional as F
+from torchtyping import TensorType as T
 
 
 def init_weights(m: nn.Module):
@@ -41,176 +40,110 @@ def positional_embeding_matrix(
     return pe
 
 
-class PositionalEncoding(nn.Module):
-    """Add positional embedings to the input"""
-
-    def __init__(self, d_embed: int, max_L: int):
-        """Initialize the layer and register a `self.te` buffer
-
-        Parameters
-        ----------
-        d_embed : int
-            length of the time vector for each timepoint; second dim of `self.te`
-        max_L : int
-            maximal time allowed by the layer; first dim of `self.te`
-        """
+class PositionalEmbedder(nn.Module):
+    def __init__(self, num_possible_tokens: int, d_embed: int):
         super().__init__()
-        self.max_L = max_L
+        self.num_possible_tokens = num_possible_tokens
+        self.d_embed = d_embed
+        emb: T["num_possible_tokens", "d_embed"] = positional_embeding_matrix(
+            num_possible_tokens, d_embed
+        )
+        self.register_buffer("emb", emb)
+
+    def forward(
+        self, tokens: T["b", "l", torch.long]
+    ) -> T["b", "l", "self.d_embed", torch.float]:  # noqa: F821
+        return self.emb[tokens]
+
+
+class Denoiser(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        len_sentence: int,
+        d_embed: int,
+        num_timestamps: int,
+        model: nn.Module,
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.len_sentence = len_sentence
         self.d_embed = d_embed
 
-        pe = positional_embeding_matrix(position_id=max_L, d_out=d_embed)
-        self.register_buffer("pe", pe)
+        self.token_embedder = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=d_embed
+        )
+        self.time_embedder = PositionalEmbedder(
+            num_possible_tokens=num_timestamps, d_embed=d_embed
+        )
+        self.model = model
 
-    def forward(self, x: T["b", "l", "d"]) -> T["b", "l", "d"]:  # noqa: F821
-        """Add positional embeding to the input
-
-        Returns
-        -------
-        torch.Tensor
-            shape (batch, l, d) where 'l' is number of tokens in each sequence,
-            and 'd' is size of each token's embeding
-        """
-
-        # pe: T["b", "d"] = self.pe[x]  # noqa: F821
-        pe: T["b", "d"] = self.pe[:, : x.size(1)]  # noqa: F821
-        return pe
+    def forward(self, x: T["b", "l", torch.long], t: T["b"]) -> T["b", "l", "v"]:  # noqa: F821
+        time: T["b", 1, "self.d_embed"] = self.time_embedder(t).unsqueeze(1)  # noqa: F821
+        tokens: T["b", "l", "self.d_embed"] = self.token_embedder(x)  # noqa: F821
+        return self.model(time + tokens)
 
 
-class SelfAttentionBlock(nn.Module):
-    def __init__(self, d_in: int, d_out_kq: int, d_out_v: int):
+class DownsampleBlock(nn.Module):
+    def __init__(self, d_in: int, dropout: float = 0.1):
         super().__init__()
-
+        kernel_size = self.kernel_size = 4
         self.d_in = d_in
-        self.d_out_kq = d_out_kq
-        self.d_out_v = d_out_v
-
-        self.Q: T["d_in", "d_out_kq"] = nn.Parameter(torch.rand(d_in, d_out_kq))
-        self.K: T["d_in", "d_out_kq"] = nn.Parameter(torch.rand(d_in, d_out_kq))
-        self.V: T["d_in", "d_out_v"] = nn.Parameter(torch.rand(d_in, d_out_v))
-
-        self.norm = nn.LayerNorm(d_out_v)
-
-    def forward(
-        self,
-        x: TensorType["batch", "n", "self.d_in"],  # noqa: F821
-    ) -> T["batch", "n", "self.d_out_v"]:  # noqa: F821
-        keys: T["batch", ..., "n", "d_out_kq"] = x @ self.K  # noqa: F821
-        queries: T["batch", ..., "n", "d_out_kq"] = x @ self.Q  # noqa: F821
-        values: T["batch", ..., "n", "d_out_v"] = x @ self.V  # noqa: F821
-        keys_T: T["batch", "d_out_kq", "n"] = keys.swapaxes(-1, -2)  # noqa: F821
-
-        attn_scores: T["batch", "n", "n"] = (  # noqa: F821
-            queries @ keys_T
-        )
-        attn_weights: T["batch", "n", "n"] = torch.softmax(  # noqa: F821
-            attn_scores / self.d_out_kq**0.5, dim=-1
+        stride = 2
+        d_out = self.d_out = d_in // stride
+        padding = stride + kernel_size - 1
+        self.layer = nn.Sequential(
+            nn.Conv1d(
+                in_channels=d_in,
+                out_channels=d_out,
+                kernel_size=kernel_size,
+                stride=2,
+                padding=padding,
+            ),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.BatchNorm1d(d_out),
         )
 
-        context_vec: T["batch", "n", "d_out_v"] = attn_weights @ values  # noqa: F821
-        return self.norm(context_vec)
+    def forward(self, x):
+        return self.layer(x)
 
 
-class TokenDenoiser(nn.Module):
-    def __init__(
-        self,
-        max_T: int,
-        max_L: int,
-        d_embed: int,
-        d_kq: int,
-        d_hidden: int,
-        n_blocks: int = 3,
-    ):
+class UpsampleBlock(nn.Module):
+    def __init__(self, d_in: int, dropout: float = 0.1):
         super().__init__()
+        self.d_in = d_in
+        d_out = d_in * 2
+        self.dropout = dropout
 
-        self.te = PositionalEncoding(
-            max_L=max_T,
-            d_embed=d_embed,
-        )
-        self.pe = PositionalEncoding(
-            max_L=max_L,
-            d_embed=d_embed,
-        )
-        self.blocks = nn.Sequential(
-            # *[
-            # SelfAttentionBlock(
-            # d_in=d_embed,
-            # d_out_kq=d_kq,
-            # d_out_v=d_hidden,
-            # )
-            # ],
-            *[
-                nn.Linear(
-                    d_embed,
-                    d_hidden,
-                )
-            ],
-            *[
-                SelfAttentionBlock(
-                    d_in=d_hidden,
-                    d_out_kq=d_kq,
-                    d_out_v=d_hidden,
-                )
-                for _ in range(n_blocks - 1)
-            ],
-            *[
-                nn.Linear(d_hidden, max_L),
-                nn.ReLU(),
-            ],
+        self.us = nn.Upsample(scale_factor=2)
+        self.nn = nn.Sequential(
+            nn.Conv1d(d_out, d_out, kernel_size=1),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.BatchNorm1d(d_out),
         )
 
-    def forward(
-        self,
-        x: T["b", "l"],  # noqa: F821
-        t: T["b"],  # noqa: F821
-    ) -> T["b", "l", "n_tokens"]:  # noqa: F821
-        te: T["b", 1, "d_embed"] = self.te(t).unsqueeze(1)  # noqa: F821
-        pe: T["b", "max_L", "d_embed"] = self.pe(x)  # noqa: F821
-        xe = te + pe
-        return self.blocks(xe)  # .swapaxes(-1, -2)
+    def forward(self, x):
+        x = self.us(x.swapaxes(-1, -2)).swapaxes(-1, -2)
+        return self.nn(x)
 
 
-class NewTokenDenoiser(nn.Module):
-    def __init__(
-        self,
-        max_T: int,
-        max_L: int,
-        d_embed: int,
-        d_hidden: int,
-        n_blocks: int = 3,
-    ):
+class ConvText(nn.Module):
+    def __init__(self, d_embed: int = 1024):
         super().__init__()
+        self.d_embed = d_embed
+        assert d_embed % 4 == 0
 
-        self.te = PositionalEncoding(
-            max_L=max_T,
-            d_embed=d_embed,
-        )
-        self.pe = PositionalEncoding(
-            max_L=max_L,
-            d_embed=d_embed,
-        )
-        self.blocks = nn.Sequential(
-            *(
-                [
-                    nn.TransformerEncoderLayer(
-                        d_model=d_embed, nhead=1, dim_feedforward=d_hidden
-                    )
-                ]
-                + [
-                    nn.TransformerEncoderLayer(
-                        d_model=d_hidden, nhead=4, dim_feedforward=d_hidden
-                    )
-                    for _ in range(n_blocks)
-                ]
-                + [nn.Linear(d_hidden, max_L), nn.ReLU()]
-            )
-        )
+        self.ds1 = DownsampleBlock(d_in=d_embed)
+        self.ds2 = DownsampleBlock(d_in=d_embed // 2)
+        self.us1 = UpsampleBlock(d_in=d_embed // 4)
+        self.us2 = UpsampleBlock(d_in=d_embed // 2)
 
-    def forward(
-        self,
-        x: T["b", "l"],  # noqa: F821
-        t: T["b"],  # noqa: F821
-    ) -> T["b", "l", "n_tokens"]:  # noqa: F821
-        te: T["b", 1, "d_embed"] = self.te(t).unsqueeze(1)  # noqa: F821
-        pe: T["b", "max_L", "d_embed"] = self.pe(x)  # noqa: F821
-        xe = te + pe
-        return self.blocks(xe)  # .swapaxes(-1, -2)
+    def forward(self, x: T["b", "d_embed", "l"]) -> T["b", "d_embed", "l"]:  # noqa: F821
+        x = x.swapaxes(-1, -2)
+        ds1 = self.ds1(x)
+        ds2 = self.ds2(ds1)
+        us1 = self.us1(ds2) + ds1
+        us2 = self.us2(us1)
+        return us2.swapaxes(-1, -2)
